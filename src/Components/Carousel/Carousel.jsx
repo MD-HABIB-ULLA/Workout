5bdd02f977c7d078dbc3452bc1bd2cdf51a759b4
habib ulla <habibulla1278@gmail.com>
2024-09-05 18:46:51 +0600

    eddited

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -2,6 +2,7 @@ import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Link } from "react-router-dom";
 
+
 const CarouselBanner = () => {
   return (
     <>
@@ -43,6 +44,7 @@ const CarouselBanner = () => {
           emulateTouch
           stopOnHover
           swipeable={false}
+
         >
           <div
             className="h-[250px] md:h-96 lg:h-screen    relative  bg-top bg-cover "
@@ -75,79 +77,7 @@ const CarouselBanner = () => {
               workout
             </h1>
           </div>
-          {/* <div
-            className="lg:h-screen relative  bg-top bg-cover "
-            style={{
-              backgroundImage: "url(https://i.ibb.co/gw55gCX/hero.jpg)",
-            }}
-          >
-            <h1 className="lg:block hidden  uppercase text-6xl mt-0 absolute rotate-90 -right-20 outline-text-gym top-1/2  -z-30">
-              workout
-            </h1>
-            <div className="h-full pt-16 ">
-              <div className="h-full   flex items-center   py-5">
-                <div className="my-auto   bg-opacity-50   text-center space-y-5  mx-auto">
-                  <div className="max-w-7xl m-auto w-full     space-y-5 lg:p-0 md:p-10 p-5">
-                    <h1 className="text-white text-center uppercase lg:text-7xl md:text-2xl text-2xl z-[10px]  ">
-                      Train Like a Beast Look <br />{" "}
-                      <span className="outline-text">Like a Beauty</span>{" "}
-                    </h1>
-                    <div className="text-white m-auto text-center w-[60%] md:block hidden ">
-                      {" "}
-                      Welcome to workout, where the night is not just for sleep;
-                      it's for sweat, strength, and self-discovery. Step into
-                      our world of after-hours fitness, where the neon lights
-                      guide your path to greatness.{" "}
-                    </div>
-                    <div>
-                      <Link to={"/classes"}>
-                        <button className=" px-4 p-2 rounded-full border bg-[#007BFF] font-bold border-[#007BFF] bt text-white hover:bg-transparent hover:text-[#007BFF] duration-500">
-                          {" "}
-                          Join Now
-                        </button>
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div
-            className="lg:h-screen relative  bg-top bg-cover "
-            style={{
-              backgroundImage: "url(https://i.ibb.co/bbHTGYd/footer-bg.jpg)",
-            }}
-          >
-            <h1 className="lg:block hidden  uppercase text-6xl mt-0 absolute rotate-90 -right-20 outline-text-gym top-1/2  -z-30">
-              workout
-            </h1>
-            <div className="h-full pt-16 ">
-              <div className="h-full   flex items-center   py-5">
-                <div className="my-auto   bg-opacity-50   text-center space-y-5  mx-auto">
-                  <div className="max-w-7xl m-auto w-full     space-y-5 lg:p-0 md:p-10 p-5">
-                    <h1 className="text-white text-center uppercase  lg:text-7xl md:text-2xl text-2xl z-[10px]  ">
-                      Train Like a Beast Look <br />{" "}
-                      <span className="outline-text">Like a Beauty</span>{" "}
-                    </h1>
-                    <div className="text-white m-auto text-center w-[60%] md:block hidden ">
-                      Welcome to workout, where the night is not just for sleep;
-                      it's for sweat, strength, and self-discovery. Step into
-                      our world of after-hours fitness, where the neon lights
-                      guide your path to greatness.
-                    </div>
-                    <div>
-                      <Link to={"/classes"}>
-                        <button className=" px-4 p-2 rounded-full border bg-[#007BFF] font-bold border-[#007BFF] bt text-white hover:bg-transparent hover:text-[#007BFF] duration-500">
-                          {" "}
-                          Join Now
-                        </button>
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div> */}
+         
         </Carousel>
       </div>
     </>
