51ed9c2057e182f447d14a8719031aab23bf637e
habib ulla <habibulla1278@gmail.com>
2024-09-05 14:04:53 +0600

    dropdown added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./node_modules/flowbite/**/*.js"],
+  content: [
+    "./index.html", 
+    "./src/**/*.{js,ts,jsx,tsx}", 
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     extend: {
       colors: {
         primary: '#007BFF',   // Blue
         secondary: '#333333', // Dark Grey
         accent: '#F0F0F0',    // Light Grey
-
       },
     },
   },
   plugins: [
-    require('flowbite/plugin')
+    require('flowbite/plugin'),
+    require('daisyui') // Add daisyUI here
   ],
+ 
 }
-
