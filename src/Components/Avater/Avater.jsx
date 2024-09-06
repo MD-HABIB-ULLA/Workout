93da92b50db071d88a882e178ce5ab34ee32440f
habib ulla <habibulla1278@gmail.com>
2024-09-06 15:43:32 +0600

    some correction added

--- src/Components/Avater/Avater.jsx
+++ src/Components/Avater/Avater.jsx
@@ -27,59 +27,7 @@ const Avater = () => {
   };
 
   return (
-    // <div className="md:relative inline-block text-left md:cursor-pointer">
-    //   {!loading ? (
-    //     <img
-    //       className="w-10 h-10 p-[1px]  rounded-full  ring-2 ring-[#007BFF] "
-    //       src={photo}
-    //       alt="Bordered avatar"
-    //       onClick={toggleDropdown}
-    //     />
-    //   ) : (
-    //     ""
-    //   )}
-
-    //   {isOpen && (
-    //     <div
-    //       id="dropdown"
-    //       className="z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 absolute mt-2 right-0"
-    //     >
-    //       <ul
-    //         className="py-2 text-sm text-gray-700 dark:text-gray-200"
-    //         aria-labelledby="dropdownDefaultButton"
-    //       >
-    //         <div className="w-full">
-    //           <img
-    //             className="w-14 h-14  rounded-full  m-auto ring-2 ring-[#007BFF] "
-    //             src={photo}
-    //             alt="Bordered avatar"
-    //           />
-    //           <p className="block cursor-default px-4 py-2  text-black text-center">
-    //             {user.displayName}
-    //           </p>
-    //           <Link
-    //             to="/dashboard"
-    //             end
-    //             className={`text-base font-bold   px-6 py-2 mt-4 w-full flex items-center   hover:bg-gray-200 dark:hover:bg-gray-700 duration-500 hover:text-gray-700 dark:hover:text-gray-200 uppercase `}
-    //           >
-    //             <CgProfile className="mr-2" /> Profile
-    //           </Link>
-    //         </div>
-    //         <li></li>
-    //         <li className="text-white bg-rose-700">
-    //           <a
-    //             href="#"
-    //             onClick={handleLogoutBtn}
-    //             className="gap-3
-    //            px-4 py-2   flex items-center"
-    //           >
-    //             <CiLogin className="text-xl " /> Sign out
-    //           </a>
-    //         </li>
-    //       </ul>
-    //     </div>
-    //   )}
-    // </div>
+    
     <div className="dropdown dropdown-end">
       <div tabIndex={0} role="button" className=" m-1">
         {!loading ? (
