318770d5ab349a0361e727c6110bfa2514c8d24e
habib ulla <habibulla1278@gmail.com>
2024-09-10 22:40:21 +0600

    updated

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import Navber from "../Components/Navber/Navber";
 import Footer from "../Components/Footer/Footer";
 import { Toaster } from "react-hot-toast";
-// import { NavbarSimple } from "../Components/Navber/MaterialNavber";
+
 
 const Main = () => {
   return (
